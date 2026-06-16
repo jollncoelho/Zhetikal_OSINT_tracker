4b1fefa0c901dcdbfba7dfc06fddeb0bd4854133
Zhétikal <66032307+jollncoelho@users.noreply.github.com>
2026-06-16 20:42:49 +0200

    Updated hermesService.tsx

--- src/hermesService.tsx
+++ src/hermesService.tsx
@@ -14,7 +14,7 @@ interface HermesGatewayResponse {
 
 // L'URL de la passerelle locale par défaut de Hermes Desktop
 // On remplace 127.0.0.1 par localhost
-const HERMES_GATEWAY_URL = 'http://localhost:63341/v1';
+const HERMES_GATEWAY_URL = 'http://localhost:11834/v1';
 
 export const queryHermes = async (messages: HermesMessage[]): Promise<string> => {
   try {
