4c1ce32f221b28da0f97d6b65be373079dbb9bdd
Zhétikal <66032307+jollncoelho@users.noreply.github.com>
2026-06-24 14:13:08 +0200

    Updated useStore.ts

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -231,7 +231,7 @@ export const useStore = create<AppState>()(
             id: crypto.randomUUID(),
             source: connection.source,
             target: connection.target,
-            type: custom',
+            type: 'custom',
             animated: true,
           };
           set((state) => ({ edges: [...state.edges, edge] }));
