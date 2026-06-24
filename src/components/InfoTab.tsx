0df5cbe4421bba18ef5b968bb9ab4790f8f901a7
Zhétikal <66032307+jollncoelho@users.noreply.github.com>
2026-06-24 15:29:41 +0200

    Updated InfoTab.tsx

--- src/components/InfoTab.tsx
+++ src/components/InfoTab.tsx
@@ -303,6 +303,7 @@ export default function InfoTab({
           onPaneClick={onPaneClick}
           nodeTypes={nodeTypes}
           edgeTypes={edgeTypes} // C'est ici que ton CustomEdge avec la croix (X) s'active !
+          defaultEdgeOptions={{ type: 'custom' }}
           connectionMode={ConnectionMode.Loose} // Permet aux flèches de s'aimanter sur n'importe quel handle
           proOptions={{ hideAttribution: true }}
           className="bg-cyber-black"
