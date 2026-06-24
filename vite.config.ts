40d108b8e1fc1dc816aaf97e674ff4388f4a225e
Zhétikal <66032307+jollncoelho@users.noreply.github.com>
2026-06-24 16:22:47 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  server: {
+    hmr: {
+      overlay: false // Désactive l'affichage rouge de blocage pour forcer l'application à tourner
+    }
+  }
 });
