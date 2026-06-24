a9df18ccb85651c61d2c87563893724056bf30a4
Zhétikal <66032307+jollncoelho@users.noreply.github.com>
2026-06-24 14:35:31 +0200

    Updated EntityNode.tsx

--- src/components/EntityNode.tsx
+++ src/components/EntityNode.tsx
@@ -91,7 +91,7 @@ export default memo(function EntityNode({ id, data, selected }: EntityNodeProps)
       }}
     >
       {/* Handles */}
-            <Handle type="target" position={Position.Left} ... />
+      <Handle type="target" position={Position.Left} ... />
       <Handle type="source" position={Position.Right} ... />
       <Handle type="target" position={Position.Top} ... />
       <Handle type="source" position={Position.Bottom} ... />
