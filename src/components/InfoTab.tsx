5773e3806d836c640db34c5fd05af90e96ad4054
Zhétikal <66032307+jollncoelho@users.noreply.github.com>
2026-06-24 13:57:02 +0200

    Updated InfoTab.tsx

--- src/components/InfoTab.tsx
+++ src/components/InfoTab.tsx
@@ -20,7 +20,6 @@ import type { CaseData, EntityData, EntityNode } from '../types';
 import NotePanel from './NotePanel';
 import EntityNodeComponent from './EntityNode';
 import SocialNodeComponent from './SocialNode';
-import CustomEdge from './CustomEdge';
 
 const nodeTypes: NodeTypes = {
   entity: EntityNodeComponent as NodeTypes['entity'],
